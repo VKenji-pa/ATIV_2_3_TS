d3ff03cd1362e634a8af772f0f4c41bf51c4b7e5
VKenji-pa <84997899+VKenji-pa@users.noreply.github.com>
2025-05-01 18:50:58 -0300

    3. Classe TestResult

--- ativTest.py
+++ ativTest.py
@@ -3,10 +3,18 @@ class TestCase:
     def __init__(self, test_method_name):
         self.test_method_name = test_method_name
 
-    def run(self):
+    def run(self, result):
+        result.test_started()
         self.set_up()    # chama método de setup
-        test_method = getattr(self, self.test_method_name)
-        test_method()    # chama método de teste 
+        
+        try:
+            test_method = getattr(self, self.test_method_name)
+            test_method()    # chama método de teste
+        except AssertionError as e:
+            result.add_failure(self.test_method_name)
+        except Exception as e:
+            result.add_error(self.test_method_name)
+            
         self.tear_down() # chama método de teardown 
 
     def set_up(self):
@@ -17,13 +25,39 @@ class TestCase:
     
 class TestSuite:
     pass
+
 class TestResult:
-    pass
+    RUN_MSG = "run"
+    FAILURE_MSG = "failed"
+    ERROR_MSG = "error"
+    
+    def __init__(self, suite_name=None):
+        self.run_count = 0
+        self.failures = []
+        self.errors = []
+        
+    def test_started(self):
+        self.run_count += 1
+    
+    def add_failure(self, test):
+        self.failures.append(test)
+        
+    def add_error(self, test):
+        self.errors.append(test)
+        
+    def summary(self):
+        return f'{self.run_count} {self.RUN_MSG}, ' \
+               f'{str(len(self.failures))} {self.FAILURE_MSG}, ' \
+               f'{str(len(self.errors))} {self.ERROR_MSG}'
+    
+
 class TestLoader:
     pass
+
 class TestRunner:   
     pass
 
+####### EXEMPLO DE USO ###
 
 class MyTest(TestCase):
 
@@ -41,12 +75,16 @@ class MyTest(TestCase):
 
     def test_c(self):
         print('test_c')
-        
+
+result = TestResult()
+     
 test = MyTest('test_a')
-test.run()
+test.run(result)
 
 test = MyTest('test_b')
-test.run()
+test.run(result)
 
 test = MyTest('test_c')
-test.run()
+test.run(result)
+
+print(result.summary())
