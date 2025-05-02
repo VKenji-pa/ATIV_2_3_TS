13c2771334438405a70b976a9a9459e886017d9f
VKenji-pa <84997899+VKenji-pa@users.noreply.github.com>
2025-05-01 21:02:02 -0300

    7. Executando Todos os Testes

---
+++ part7.py
@@ -0,0 +1,14 @@
+from testFrame import *
+
+loader = TestLoader()
+test_case_suite = loader.make_suite(TestCaseTest)
+test_suite_suite = loader.make_suite(TestSuiteTest)
+test_loader_suite = loader.make_suite(TestLoaderTest)
+
+suite = TestSuite()
+suite.add_test(test_case_suite)
+suite.add_test(test_suite_suite)
+suite.add_test(test_loader_suite)
+
+runner = TestRunner()
+runner.run(suite)
